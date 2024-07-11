8781362fd213e285f01f631dbb1ee395cc9a2cbe
oovaa <omarvx211@gmail.com>
2024-07-11 17:17:06 +0200

    task 10 done the basic auth is completed at 11-17

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import base64
+from typing import TypeVar
 from exceptiongroup import catch
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -32,3 +34,26 @@ class BasicAuth(Auth):
                 ':' not in decoded_base64_authorization_header:
             return None, None
         return tuple(decoded_base64_authorization_header.split(':'))
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        auth_header = self.authorization_header(request)
+        get_base64 = self.extract_base64_authorization_header(auth_header)
+        decode_base64 = self.decode_base64_authorization_header(get_base64)
+        get_data = self.extract_user_credentials(decode_base64)
+        user_obj = self.user_object_from_credentials(get_data[0], get_data[1])
+
+        return user_obj
