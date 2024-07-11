1a7392846381d05e218fb854ca929b6d3c1b1de3
oovaa <omarvx211@gmail.com>
2024-07-11 17:29:14 +0200

    task 11 advance done it was easy tho at 11-17

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,7 +33,7 @@ class BasicAuth(Auth):
                 not isinstance(decoded_base64_authorization_header, str) or \
                 ':' not in decoded_base64_authorization_header:
             return None, None
-        return tuple(decoded_base64_authorization_header.split(':'))
+        return decoded_base64_authorization_header.split(':', 1)
 
     def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
         if user_email is None or not isinstance(user_email, str):
